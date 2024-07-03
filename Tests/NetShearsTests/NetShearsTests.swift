83f48d0ab6c3f01134c25ece43fa52e29864dadf
Rafael Francisco <rfrancisco@eneida.io>
2024-07-03 17:04:06 +0100

    Start fixing tests

--- Tests/NetShearsTests/NetShearsTests.swift
+++ Tests/NetShearsTests/NetShearsTests.swift
@@ -6,17 +6,24 @@ final class NetShearsTests: XCTestCase {
     
     override func setUpWithError() throws {
         try super.setUpWithError()
-        NetShears.shared.startInterceptor()
+        
+        Task {
+            await NetShears.shared.startInterceptor()
+        }
     }
     
     override func tearDownWithError() throws {
         PersistHelper.clear()
-        NetShears.shared.stopInterceptor()
-        Storage.shared.clearRequests()
+        
+        Task {
+            await NetShears.shared.stopInterceptor()
+            await Storage.shared.clearRequests()
+        }
+        
         try super.tearDownWithError()
     }
     
-    func testModifyResponse() {
+    func testModifyResponse() async {
         let modifier = RequestEvaluatorModifierResponse(response: .init(
             url: "https://example.com/",
             data: #"{"message": "ok"}"#.data(using: .utf8)!,
@@ -24,7 +31,7 @@ final class NetShearsTests: XCTestCase {
             headers: ["result": "true"]
         ))
         
-        NetShears.shared.modify(modifier: modifier)
+        await NetShears.shared.modify(modifier: modifier)
         
         var request = URLRequest(url: URL(string: "https://example.com/")!)
         request.httpMethod = "POST"
@@ -37,17 +44,17 @@ final class NetShearsTests: XCTestCase {
             self.expectation.fulfill()
         }.resume()
         
-        wait(for: [expectation], timeout: 1)
+        await fulfillment(of: [expectation], timeout: 1)
     }
     
-    func testModifyRequestBeforeModifiyingResponse() {
+    func testModifyRequestBeforeModifiyingResponse() async {
         let modifier = RequestEvaluatorModifierResponse(response: .init(
             url: "https://example.com/sandbox/ok",
             data: #"{"message": "ok"}"#.data(using: .utf8)!
         ))
         
-        NetShears.shared.modify(modifier: RequestEvaluatorModifierEndpoint(redirectedRequest: .init(originalUrl: "v1", redirectUrl: "sandbox")))
-        NetShears.shared.modify(modifier: modifier)
+        await NetShears.shared.modify(modifier: RequestEvaluatorModifierEndpoint(redirectedRequest: .init(originalUrl: "v1", redirectUrl: "sandbox")))
+        await NetShears.shared.modify(modifier: modifier)
         
         let task = URLSession.shared.dataTask(with: URL(string: "https://example.com/v1/ok")!) { data, response, _ in
             let dict = try! JSONSerialization.jsonObject(with: data!, options: .fragmentsAllowed) as! [String: String]
@@ -58,28 +65,28 @@ final class NetShearsTests: XCTestCase {
         
         task.resume()
         
-        wait(for: [expectation], timeout: 1)
+        await fulfillment(of: [expectation], timeout: 1)
     }
     
-    func testMonitorModifiedResponseRequest() {
-        NetShears.shared.startLogger()
+    func testMonitorModifiedResponseRequest() async {
+        await NetShears.shared.startLogger()
         
         let modifier = RequestEvaluatorModifierResponse(response: .init(
             url: "https://example.com/",
             data: #"{"message": "ok"}"#.data(using: .utf8)!
         ))
         
-        NetShears.shared.modify(modifier: modifier)
+        await NetShears.shared.modify(modifier: modifier)
         
         URLSession.shared.dataTask(with: URL(string: "https://example.com/")!) { data, response, _ in
             XCTAssertTrue(Storage.shared.requests.contains(where: { $0.url == modifier.response.url }))
             self.expectation.fulfill()
         }.resume()
         
-        wait(for: [expectation], timeout: 1)
+        await fulfillment(of: [expectation], timeout: 1)
     }
 
-    func testIgnoreOnUrlRequest() {
+    func testIgnoreOnUrlRequest() async {
         //given
         let url = "https://example.com/"
         NetShears.shared.ignore = .enabled(ignoreHandler: { request in
@@ -87,17 +94,18 @@ final class NetShearsTests: XCTestCase {
         })
 
         //when
-        NetShears.shared.startLogger()
+        await NetShears.shared.startLogger()
         URLSession.shared.dataTask(with: URL(string: url)!) { data, response, _ in
             //then
             XCTAssertTrue(Storage.shared.requests.contains(where: { $0.url == url }))
-            XCTAssertTrue(Storage.shared.filteredRequests.isEmpty)
+            XCTAssertTrue(Storage.shared.filteredRequests().isEmpty)
             self.expectation.fulfill()
         }.resume()
 
-        wait(for: [expectation], timeout: 2)
+        await fulfillment(of: [expectation], timeout: 1)
     }
 
+    @NetShearsActor
     func testIgnoreOnHeaderRequest() {
         //given
         let headerKey = "language"
@@ -118,7 +126,7 @@ final class NetShearsTests: XCTestCase {
             XCTAssertTrue(Storage.shared.requests.contains(where: { $0.headers.contains { (key,value) in
                 key == headerKey && value == headerValue
             }}))
-            XCTAssertTrue(Storage.shared.filteredRequests.isEmpty)
+            XCTAssertTrue(Storage.shared.filteredRequests().isEmpty)
             self.expectation.fulfill()
         }.resume()
 
